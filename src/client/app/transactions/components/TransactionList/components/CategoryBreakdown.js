113ee38c29deef57113066d24adc10e56bf854ac
Jeffrey Russom <jeffreyrussom@gmail.com>
2017-05-21 22:23:50 -0500

    Added category monthly breakdown

--- src/client/app/transactions/components/TransactionList/components/CategoryBreakdown.js
+++ src/client/app/transactions/components/TransactionList/components/CategoryBreakdown.js
@@ -3,16 +3,33 @@ import Categories from '../../../../../../server/constants/categories';
 import RaisedButton from 'material-ui/RaisedButton';
 import COLORS from '../../../../shared/colors';
 
-const CategoryBreakdown = ({statistics}) => (
+const amountLabelStyle = {
+    width: '30%',
+    display: 'inline-block',
+    color: '#FFFFFF'
+};
+
+const categoryLabelStyle = {
+    width: '70%',
+    display: 'inline-block',
+    textAlign: 'left',
+    color: '#FFFFFF'
+};
+
+console.log(RaisedButton.propTypes);
+const CategoryBreakdown = ({statistics, onHover}) => (
     <div>
         {Object.keys(Categories).filter(c => statistics[c] > 0).map((c, i) =>
             <RaisedButton 
+                onMouseEnter={() => onHover(c, i)}
+                onMouseLeave={() => onHover(c, -1)}
                 key={i} 
-                label={`\$${parseInt(statistics[c], 10)} ${Categories[c]}`} 
-                labelColor={'#FFFFFF'}
                 backgroundColor={COLORS[i % COLORS.length]} 
                 style={{width: 'calc(33% - 20px)', 
-                margin: '10px'}}/>
+                margin: '10px'}}>
+                <div style={amountLabelStyle}>$ {parseInt(statistics[c], 10)}</div>
+                <div style={categoryLabelStyle}>{Categories[c]}</div>
+            </RaisedButton>
         )}
     </div>
 );
--- src/client/app/transactions/components/TransactionList/components/MonthSummary.js
+++ src/client/app/transactions/components/TransactionList/components/MonthSummary.js
@@ -1,24 +1,49 @@
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
+import { PieChart, Pie, Cell, ResponsiveContainer, Sector } from 'recharts'
 import React from 'react'
 import { computeStatistics } from '../../../helpers'
 import CategoryBreakdown from './CategoryBreakdown';
 import COLORS from '../../../../shared/colors';
 
+const renderActiveShape = ({outerRadius, ...props}) => {
+  return (
+      <Sector
+        {...props}
+        outerRadius={outerRadius + 10}
+      />
+  );
+};
+
+
 class MonthSummary extends React.Component {
+    constructor() {
+        super();
+        this.state = {activeIndex: -1};
+    }
+
+    onPieEnter = (data, index) => {
+        this.setState({
+            activeIndex: index
+        });
+    };
+
     render() {
        
         const statistics = computeStatistics(this.props.transactions);
-        const data = Object.keys(statistics).map(c => ({name: c, value: parseInt(statistics[c], 10)}));
+        const data = Object.keys(statistics).filter(c => statistics[c] > 0).map(c => ({name: c, value: parseInt(statistics[c], 10)}));
         if (data && data.length > 0) {
           	return (
                 <div style={{display: 'flex', minHeight: '250px'}}>
                     <div style={{flexGrow: '1'}}>
-                        <CategoryBreakdown statistics={statistics}/>
+                        <CategoryBreakdown statistics={statistics} activeIndex={this.state.activeIndex} onHover={this.onPieEnter}/>
                     </div>
-                    <PieChart
+                    <PieChart onMouseEnter={this.onPieEnter}
+                       
                         width={300}
                         height={200}>
-                        <Pie
+                        <Pie 
+                         activeIndex={this.state.activeIndex}
+                         isAnimationActive={false}
+                        activeShape={renderActiveShape}
                         data={data} 
                         label={false}
                         fill="#8884d8"
